526403b52804d3737a1da6bd4a73c62367ffd294
The Mighty Quinn <quinn@themightyquinn.com>
2011-01-09 13:42:24 -0800

    Beta 1.0

--- src/canvas.js
+++ src/canvas.js
@@ -1,47 +1,80 @@
-var BOX_SIZE = 6;
+var BOX_SIZE = 10;
 
-var canvas = $('#canvas').get(0);
-console.log(canvas);
-var ctx = canvas.getContext('2d');
-var cp1x = 50,
-	cp1y = -50,
-	cp2x = 150,
-	cp2y = -50,
-	x = 200,
-	y = 0;
-
-//move context to center x
-ctx.translate(0,100);
-
-//draw control points
-ctx.save();
-var haff = -BOX_SIZE/2;
-ctx.translate(haff, haff);
-ctx.strokeStyle = "red";
-ctx.strokeRect(cp1x,cp1y,BOX_SIZE,BOX_SIZE);
-ctx.strokeRect(cp2x,cp2y,BOX_SIZE,BOX_SIZE);
-ctx.restore();
-
-//draw control lines
-ctx.save();
-ctx.strokeStyle = "grey";
-ctx.moveTo(cp1x,cp1y);
-ctx.lineTo(0,0);
-ctx.moveTo(cp2x,cp2y);
-ctx.lineTo(x,y);
-ctx.stroke();
-ctx.restore();
-
-
-ctx.beginPath();
-ctx.moveTo(0,0);
-ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, x, y);
-ctx.stroke();
-
-$(canvas).bind('mousedown', function (e) {
+function drawBCurve () {
 	
-	$(canvas).bind('mousemove', function (e) {
-		console.log(e);
-	});
+	var haff = -BOX_SIZE/2,
+		x = canvas.width,
+		y = 0;
 	
-});
+	var ctx = canvas.getContext('2d');
+	
+	ctx.clearRect(0,0,canvas.width, canvas.height);
+	
+	ctx.save()
+	//move context to center x
+	ctx.translate(0,canvas.height/2);
+	
+	//draw control points
+	ctx.save();
+	ctx.translate(haff, haff);
+	ctx.strokeStyle = "red";
+	ctx.strokeRect(cp1.x,cp1.y,BOX_SIZE,BOX_SIZE);
+	ctx.strokeRect(cp2.x,cp2.y,BOX_SIZE,BOX_SIZE);
+	ctx.restore();
+	
+	//draw control lines
+	ctx.save();
+	ctx.strokeStyle = "grey";
+	ctx.moveTo(cp1.x,cp1.y);
+	ctx.lineTo(0,0);
+	ctx.moveTo(cp2.x,cp2.y);
+	ctx.lineTo(x,y);
+	ctx.stroke();
+	ctx.restore();
+	
+	
+	ctx.beginPath();
+	ctx.moveTo(0,0);
+	ctx.bezierCurveTo(cp1.x, cp1.y, cp2.x, cp2.y, x, y);
+	ctx.stroke();
+	
+	ctx.restore();
+}
+
+/**
+ * Main
+ */
+var cp1 = {x:50,y:-50};
+var cp2 = {x:150,y:-50};
+
+var cpDragging = null;
+
+var canvas = $('#canvas').bind({
+	mousedown: function (e) {
+		//check for bounds of cp.
+		var locX = e.offsetX + BOX_SIZE/2;
+		var locY = (e.offsetY - 100) + BOX_SIZE/2;
+		
+		$.each([cp1, cp2], function (index, cp) {
+			if (locX - cp.x > 0 && locX - cp.x < BOX_SIZE && locY - cp.y > 0 && locY - cp.y < BOX_SIZE ) {
+				cpDragging = cp;
+				cp.x = e.offsetX;
+				cp.y = e.offsetY - 100;
+				drawBCurve();
+				return false;
+			}
+		});
+	},
+	mousemove: function (e) {
+		if (cpDragging) {
+			cpDragging.x = e.offsetX;
+			cpDragging.y = e.offsetY - 100;
+			drawBCurve();
+		}
+	},
+	mouseup: function (e) {
+		cpDragging = null;
+	}
+}).get(0);
+
+drawBCurve(50,-50, 150, -50);
